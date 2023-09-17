7ea942d49fdf8029db5aeb4f99a9c8bcdff7c57c
Alena <crimeaodessa2006@gmail.com>
2023-09-17 22:15:33 +0200

    finish

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # galleryBootstrap
+
+## Galeria zdjęć
+W pierwszej galerii zdjęć, po najechaniu kursorem na zdjęcie, pojawia się szkło powiększające. Kliknięcie na zdjęcie powoduje jego powiększenie i wypełnienie całego ekranu. Kliknięcie poza obszarem zdjęcia zamyka zdjęcie.
+
+## Galeria zdjęć slider
+W drugiej galerii zdjęć naciśnięcie prawego lub lewego przycisku powoduje przesunięcie zdjęć w odpowiednim kierunku. Slider nie ma końca. Zdjęcie środkowe jest zawsze powiększone.
+
+## Galeria zdjęć wydarzenie
+W galerii zdjęć szpitala naciśnięcie prawego lub lewego przycisku powoduje przesunięcie zdjęć w odpowiednim kierunku. Slider nie ma końca. Możesz także zmieniać obrazy, klikając miniatury na dole bloku.
--- docs/script.js
+++ docs/script.js
@@ -1,97 +1,3 @@
-// "use strict";
-
-// let second;
-
-// const arr = [
-//     `<img src="/pictures/1.webp" alt="Pictures_1"/>`,
-//     `<img src="/pictures/2.webp" alt="Pictures_2"/>`,
-//     `<img src="/pictures/3.webp" alt="Pictures_3"/>`
-// ];
-
-// const container = document.createElement("container");
-// container.className = "list_img";
-
-// function creat() {
-//     arr.forEach((element) => {
-//         let div = document.createElement("div");
-//         div.innerHTML = `${element}`;
-//         div.className = "img";
-//         if (element === arr[1]) div.className = "img activeSlider";
-//         container.append(div);
-//     });
-//     document.querySelector('.scroll_gallery').append(container);
-// }
-
-// creat(arr);
-
-// document.getElementById('right').addEventListener('click', () => {
-//     scroll(rightScroll, "-220px")
-// })
-
-// document.getElementById('left').addEventListener('click', () => {
-//     scroll(leftScroll, "220px")
-// })
-
-// function rightScroll() {
-//     let elm = arr.shift(0);
-//     arr.push(elm);
-//     container.innerHTML = '';
-//     creat(arr);
-//     second = document.querySelector('.activeSlider');
-//     scrollUl("220px");
-// }
-
-// function leftScroll() {
-//     let elm = arr.pop(3);
-//     arr.unshift(elm);
-//     container.innerHTML = '';
-//     creat(arr);
-//     second = document.querySelector('.activeSlider');
-//     scrollUl("-220px");
-// }
-
-// function scroll(side, y) {
-//     second = document.querySelector('.activeSlider');
-//     second.animate([
-//         { transform: 'scale(1.3)' },
-//         { transform: 'scale(1)' },
-//     ], 1000);
-//     container.animate([
-//         { transform: 'translate(0, 0)', opacity: 1 },
-//         { transform: `translate(${y}, 0)`, opacity: 0 }
-//     ], 1000);
-//     setTimeout(side, 500);
-// }
-
-// function scrollUl(y) {
-//     container.animate([
-//         { transform: `translate(${y}, 0)`, opacity: 0 },
-//         { transform: 'translate(0, 0)', opacity: 1 }
-//     ], 1000);
-//     second.animate([
-//         { transform: 'scale(1)' },
-//         { transform: 'scale(1.3)' },
-//     ], 1000);
-// }
-
-// function rightScroll() {
-//     let elm = arr.shift(0);
-//     arr.push(elm);
-//     container.innerHTML = '';
-//     creat(arr);
-//     second = document.querySelector('.activeSlider');
-//     scrollUl("220px");
-// }
-
-// function leftScroll() {
-//     let elm = arr.pop(3);
-//     arr.unshift(elm);
-//     container.innerHTML = '';
-//     creat(arr);
-//     second = document.querySelector('.activeSlider');
-//     scrollUl("-220px");
-// }
-
 const bigImgHospital = document.querySelector('.img_hospital_big');
 const imgsHospital = document.querySelectorAll('.img_hospital');
 const carousel = document.getElementById("carouselExampleControls");
@@ -101,11 +7,9 @@ let currentImageIndex = 0;
 
 function updateBigImageAndClasses() {
   const currentImage = imgsHospital[currentImageIndex];
-  bigImgHospital.innerHTML = `<img src="${currentImage.src}" class="d-block w-100 img_big" alt="${currentImage.alt}">`;
-  imgsHospital.forEach(function(img) {
-    img.classList.remove("active");
-  });
-  currentImage.classList.add("active");
+  bigImgHospital.innerHTML = `<img src="${currentImage.src}" class="d-block w-100 img_big" alt="${currentImage.alt}"> <div class="imgTitle">${currentImage.alt}</div>`;
+  imgsHospital.forEach(img => img.classList.remove("active"));
+  imgsHospital[currentImageIndex].classList.add("active");
 }
 
 document.querySelector(".hospital").addEventListener("click", (event) => {
@@ -115,55 +19,61 @@ document.querySelector(".hospital").addEventListener("click", (event) => {
   }
 });
 
-prevButton.addEventListener("click", function() {
-  currentImageIndex--;
-  if (currentImageIndex < 0) {
-    currentImageIndex = imgsHospital.length - 1;
-  }
+prevButton.addEventListener("click", () => {
+  currentImageIndex = (currentImageIndex - 1 + imgsHospital.length) % imgsHospital.length;
   updateBigImageAndClasses();
 });
 
-nextButton.addEventListener("click", function() {
-  currentImageIndex++;
-  if (currentImageIndex >= imgsHospital.length) {
-    currentImageIndex = 0;
-  }
+nextButton.addEventListener("click", () => {
+  currentImageIndex = (currentImageIndex + 1) % imgsHospital.length;
   updateBigImageAndClasses();
 });
 
 updateBigImageAndClasses();
 
-const slider = document.querySelector('.slider');
-const slides = document.querySelectorAll('.img');
-const prevButtonSecond = document.querySelector('.prev-button');
-const nextButtonSecond = document.querySelector('.next-button');
-let currentIndex = 1;
+const btnNext = document.querySelector(".next-button");
+const btnPrev = document.querySelector(".prev-button");
+const slides = document.querySelectorAll(".slideSecond");
+const sliders = [];
 
-function showSlide(index) {
-    slides.forEach((slide, i) => {
-        slide.style.transform = `translateX(220px)`;
-        if (i === index) {
-            slide.classList.add('current');
-        } else {
-            slide.classList.remove('current');
-        }
-    });
+function draw() {
+  document.querySelector(".slider").innerHTML = ""; 
+  for (let i = currentImageIndex; i < currentImageIndex + 3; i++) {
+    let slideIndex = i % slides.length;
+    let slide = document.createElement("div");
+    slide.classList.add("slideSecond");
+    slide.style.left = (i - currentImageIndex) * 100 + "%";
+    slide.appendChild(sliders[slideIndex]);
+    if (i - currentImageIndex === 1) {
+      slide.classList.add("current");
+    }
+    document.querySelector(".slider").appendChild(slide);
+  }
 }
 
-function prevSlide() {
-    currentIndex = (currentIndex - 1 + slides.length) % slides.length;
-    showSlide(currentIndex);
-}
+btnNext.addEventListener("click", () => {
+  currentImageIndex = (currentImageIndex + 1) % imgsHospital.length;
+  draw();
+});
 
-function nextSlide() {
-    currentIndex = (currentIndex + 1) % slides.length;
-    showSlide(currentIndex);
-    // slider.style.transform = `translateX(-220px)`;
-}
+btnPrev.addEventListener("click", () => {
+  currentImageIndex = (currentImageIndex - 1 + imgsHospital.length) % imgsHospital.length;
+  draw();
+});
 
-prevButtonSecond.addEventListener('click', prevSlide);
-nextButtonSecond.addEventListener('click', nextSlide);
+slides.forEach((slide) => {
+  sliders.push(slide.querySelector("img"));
+});
 
-// showSlide(currentIndex);
+draw();
 
+const images = document.querySelectorAll('.image-zoom');
+const modalImage = document.querySelector('.modalImage');
 
+images.forEach((image) => {
+  image.addEventListener('click', () => {
+    modalImage.src = image.src;
+    const imageModal = new bootstrap.Modal(document.getElementById('imageModal'));
+    imageModal.show();
+  });
+});
